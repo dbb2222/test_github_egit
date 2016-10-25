a86b3d83aa7fb4339febddbb0735563b0726a414
dbb2222 <dbb2222@gmail.com>
2016-10-25 14:10:53 +0300

    Feature 4 - branch1

--- src/main/java/test_github_egit/Test_github_egit.java
+++ src/main/java/test_github_egit/Test_github_egit.java
@@ -6,6 +6,7 @@ public class Test_github_egit {
 		System.out.println("Feature 1");
 		System.out.println("Feature 2");
 		System.out.println("Feature 3");
+		System.out.println("Feature 4 - branch1");
 
 	}
 

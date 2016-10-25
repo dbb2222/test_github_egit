2994948b8616f6b3646775e2270bb728cafedd71
dbb2222 <dbb2222@gmail.com>
2016-10-25 12:56:11 +0300

    Feature 3

--- src/main/java/test_github_egit/Test_github_egit.java
+++ src/main/java/test_github_egit/Test_github_egit.java
@@ -5,6 +5,7 @@ public class Test_github_egit {
 	public static void main(String[] args) {
 		System.out.println("Feature 1");
 		System.out.println("Feature 2");
+		System.out.println("Feature 3");
 
 	}
 
